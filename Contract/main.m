baea7f5251307da819ea3561f85e9ef69e7150d7
aprillv <April@Aprils-Mac-mini.local>
2015-11-18 11:26:16 +0800

    Creat

---
+++ Contract/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contract
+//
+//  Created by April on 11/18/15.
+//  Copyright © 2015 HapApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
