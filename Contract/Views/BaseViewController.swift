6c993511b79ae3274760ff75f58062ff6cf95ccf
aprillv <xiujun007@gmail.com>
2015-11-20 08:49:37 +0800

    11.20

--- Contract/Views/BaseViewController.swift
+++ Contract/Views/BaseViewController.swift
@@ -13,7 +13,17 @@ class BaseViewController: UIViewController {
     private struct ProjectConstants{
         static let MsgTitle : String = "BA Contract"
         static let MsgOKTitle : String = "OK"
+        static let MsgValidationTitle : String = "Validation Failed"
     }
+    
+    func IsNilOrEmpty(str : String?) -> Bool{
+        if str == nil || str == "" {
+            return true
+        }else {
+            return false
+        }
+    }
+    
     func PopMsgWithJustOK(msg msg1: String, txtField : UITextField?){
         
         let alert: UIAlertController = UIAlertController(title: ProjectConstants.MsgTitle, message: msg1, preferredStyle: .Alert)
@@ -31,5 +41,28 @@ class BaseViewController: UIViewController {
         
         
     }
+    
+    func PopMsgValidationWithJustOK(msg msg1: String, txtField : UITextField?){
+        
+        let alert: UIAlertController = UIAlertController(title: ProjectConstants.MsgValidationTitle, message: msg1, preferredStyle: .Alert)
+        
+        //Create and add the OK action
+        let oKAction: UIAlertAction = UIAlertAction(title: ProjectConstants.MsgOKTitle, style: .Cancel) { action -> Void in
+            //Do some stuff
+            txtField?.becomeFirstResponder()
+        }
+        alert.addAction(oKAction)
+        
+        
+        //Present the AlertController
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
 
 }
+
+
+
--- Contract/Views/LoginViewController.swift
+++ Contract/Views/LoginViewController.swift
@@ -8,36 +8,149 @@
 
 import UIKit
 
-class LoginViewController: BaseViewController {
+class LoginViewController: BaseViewController, UITextFieldDelegate {
 
     private struct constants{
         static let PasswordEmptyMsg : String = "Password Required."
         static let EmailEmptyMsg :  String = "Email Required."
+        static let WrongEmailOrPwdMsg :  String = "Your Email or password is incorrect."
+        
+        static let BorderColor : UIColor = UIColor(red: 204/255.0, green: 204/255.0, blue: 204/255.0, alpha: 1)
+        
+        static let UserInfoRememberMe :  String = "Login Remember Me"
+        static let UserInfoEmail :  String = "Login Email"
+        static let UserInfoPwd :  String = "Login Password"
+        
+        static let SegueToAddressList :  String = "adressList"
     }
-    @IBOutlet weak var emailTxt: UITextField!
-    @IBOutlet weak var passwordTxt: UITextField!
-    @IBOutlet weak var rememberMeSwitch: UISwitch!
+    
+    @IBOutlet weak var emailTxt: UITextField!{
+        didSet{
+            emailTxt.returnKeyType = .Next
+            emailTxt.delegate = self
+            let userInfo = NSUserDefaults.standardUserDefaults()
+            emailTxt.text = userInfo.objectForKey(constants.UserInfoEmail) as? String
+        }
+    }
+    @IBOutlet weak var passwordTxt: UITextField!{
+        didSet{
+            passwordTxt.returnKeyType = .Go
+            passwordTxt.enablesReturnKeyAutomatically = true
+            passwordTxt.delegate = self
+            let userInfo = NSUserDefaults.standardUserDefaults()
+            if let isRemembered = userInfo.objectForKey(constants.UserInfoRememberMe) as? Bool{
+                if isRemembered {
+                    passwordTxt.text = userInfo.objectForKey(constants.UserInfoPwd) as? String
+                }
+                
+            }
+        }
+    }
+    @IBOutlet weak var signInBtn: UIButton!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        switch textField{
+        case emailTxt:
+            passwordTxt.becomeFirstResponder()
+        case passwordTxt:
+            Login(signInBtn)
+        default:
+            break
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        setSignInBtn()
+    }
+    
+    private func setSignInBtn(){
+        signInBtn.enabled = !self.IsNilOrEmpty(passwordTxt.text)
+            && !self.IsNilOrEmpty(emailTxt.text)
+    }
+    @IBOutlet weak var rememberMeSwitch: UISwitch!{
+        didSet {
+            rememberMeSwitch.transform = CGAffineTransformMakeScale(0.9, 0.9)
+            let userInfo = NSUserDefaults.standardUserDefaults()
+            if let isRemembered = userInfo.objectForKey(constants.UserInfoRememberMe) as? Bool{
+                rememberMeSwitch.on = isRemembered
+            }else{
+                rememberMeSwitch.on = true
+            }
+        }
+    }
+    
+    @IBOutlet weak var backView: UIView!{
+        didSet{
+            backView.backgroundColor = UIColor.whiteColor()
+            backView.layer.borderColor = constants.BorderColor.CGColor
+            backView.layer.borderWidth = 1.0
+            backView.layer.cornerRadius = 8
+        }
+    }
     
     @IBAction func Login(sender: UIButton) {
-        
         emailTxt.resignFirstResponder()
         passwordTxt.resignFirstResponder()
         
         let email = emailTxt.text
         let password = passwordTxt.text
         
-        if email == nil || email == "" {
+        if IsNilOrEmpty(email) {
             self.PopMsgWithJustOK(msg: constants.EmailEmptyMsg, txtField: emailTxt)
         }else{
-            if password == nil || password == "" {
+            if IsNilOrEmpty(password) {
                 self.PopMsgWithJustOK(msg: constants.PasswordEmptyMsg, txtField: passwordTxt)
             }else {
                 // do login
+                signInBtn.hidden = true
+                emailTxt.enabled = false
+                passwordTxt.enabled = false
+                emailTxt.textColor = UIColor.darkGrayColor()
+                passwordTxt.textColor = UIColor.darkGrayColor()
+                spinner.startAnimating()
+                saveEmailAndPwdToDisk(email: email!, password: password!)
+                self.performSegueWithIdentifier(constants.SegueToAddressList, sender: self)
             }
+        }
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+                case constants.SegueToAddressList:
+                    print("go to address list")
+                break
+            default:
+                break
+            }
+        }
+        
+    }
+    
+    func saveEmailAndPwdToDisk(email email: String, password: String){
+        let userInfo = NSUserDefaults.standardUserDefaults()
+        if rememberMeSwitch.on {
+            userInfo.setObject(true, forKey: constants.UserInfoRememberMe)
+            userInfo.setObject(email, forKey: constants.UserInfoEmail)
+            userInfo.setObject(password, forKey: constants.UserInfoPwd)
+        }else{
+            userInfo.setObject(false, forKey: constants.UserInfoRememberMe)
+            userInfo.setObject(email, forKey: constants.UserInfoEmail)
+            userInfo.setObject("", forKey: constants.UserInfoPwd)
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+       setSignInBtn()
+        
+        
+
+        
         // Do any additional setup after loading the view.
     }
 
@@ -56,5 +169,6 @@ class LoginViewController: BaseViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 
 }
